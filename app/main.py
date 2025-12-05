c20472eeeea2a165bed9a36b51b439432c81acda
Manar Elhabbal <manarelhabbal619@gmail.com>
2025-12-05 15:38:15 +0200

    implement echo done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 import sys
 
-
+commands = {
+    "exit": lambda code=0, *_: sys.exit(int(code)),
+    "echo": lambda *args: print(" ".join(args)),
+}
 def main():
     while(True):
         sys.stdout.write("$ ")
-        command = input()
-        if(command == 'exit'):
-            break
-        print(f"{command}: command not found")
+        sys.stdout.flush()
+
+        user_input = input().split()
+
+        command =user_input[0]
+        args =user_input[1:]
+
+        if command in commands:
+            commands[command](*args)
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
